676bae6d16673f31c9e5fdcec2e22a2288a4c619
Ganaraja <ganaraja@gmail.com>
2016-01-20 21:31:42 +0530

    Assignment 34 - Corrections

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    @post = Post.create(post_params)
 
     @topic = Topic.find(params[:topic_id])
     @post.topic = @topic
@@ -56,4 +54,8 @@ class PostsController < ApplicationController
    end
  end
 
+ private
+   def post_params
+     params[:post].permit[:title]
+   end
 end
--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -13,10 +13,7 @@ class SponsoredPostsController < ApplicationController
   end
 
   def create
-    @sponsored_post = SponsoredPost.new
-    @sponsored_post.title = params[:sponsored_post][:title]
-    @sponsored_post.body = params[:sponsored_post][:body]
-    @sponsored_post.price = params[:sponsored_post][:price]
+    @sponsored_post = SponsoredPost.create(sponsored_post_params)
 
     @topic = Topic.find(params[:topic_id])
     @sponsored_post.topic = @topic
@@ -56,4 +53,9 @@ class SponsoredPostsController < ApplicationController
       render :show
     end
   end
+
+  private
+    def sponsored_post_params
+      params[:sponsored_post].permit[:title]
+    end
 end
