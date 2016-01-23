d3c95ccec0de14a7a41bec8400c624a4de6f557c
Ganaraja <ganaraja@gmail.com>
2016-01-23 15:27:30 +0530

    Assignment 35

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,7 +3,12 @@ include RandomData
 
 RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  it { should have_many(:posts) }
+  it { should validate_presence_of(:name)}
+  it { should validate_presence_of(:description)}
 
+  it {should validate_length_of(:name).is_at_least(5)}
+  it {should validate_length_of(:description).is_at_least(15)}
   describe "attributes" do
     it {should have_many(:posts)}
     it "should respond to name" do
@@ -18,7 +23,7 @@ RSpec.describe Topic, type: :model do
       expect(topic).to respond_to(:public)
     end
 
-    it "shoudl be true by default" do
+    it "should be true by default" do
       expect(topic.public).to be(true)
     end
   end
