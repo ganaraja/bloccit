2e561ec362915aa5327b3a5b93a75b88de6ea7a4
Ganaraja <ganaraja@gmail.com>
2016-02-28 12:18:29 +0530

    Assignment 43

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,7 @@ class Post < ActiveRecord::Base
   validates :body, length: {minimum: 20}, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+  after_create :create_vote
 
   def label_spam
     self.title = self.title + "SPAM"
@@ -34,5 +35,10 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+private
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 end
