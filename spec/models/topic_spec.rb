fe9822b66c0502b9875bd0cfc7293af060a83069
Ganaraja <ganaraja@gmail.com>
2016-03-20 11:55:24 +0530

    Assignment 46

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -33,7 +33,19 @@ RSpec.describe Topic, type: :model do
      @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, public: false)
    end
 
-   describe "visible_to(user)" do
+  describe "publically viewable" do
+    it "returns only public topics" do
+      expect(Topic.publically_viewable).to eq([@public_topic])
+    end
+  end
+
+  describe "privately viewable" do
+    it "returns only public topics" do
+      expect(Topic.privately_viewable).to eq([@private_topic])
+    end
+  end
+
+  describe "visible_to(user)" do
      it "returns all topics if the user is present" do
        user = User.new
        expect(Topic.visible_to(user)).to eq(Topic.all)
